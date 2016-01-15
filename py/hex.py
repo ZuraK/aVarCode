296cfad58689dca9f4c7b9cc69f18aae363f4f47
Michael Åberg <ZuraK@users.noreply.github.com>
2016-01-15 07:29:49 +0100

    Update hex.py

--- py/hex.py
+++ py/hex.py
@@ -1,7 +1,31 @@
+# File:
+# Desc:
 
-# HEX Code
+import math;
 
-def GenerateHEX():
-  # code
-  return HexData;
+# param: hType, hexType, 0 for Flat topped, 30 if Pointy topped
+# param: center, Vector2Point, hex center
+# param: radius, size of hex
+# param: index, indexPoint corner of hex, 0-5
+# returns: Vector2Point hex corner
+def GeneratePointHEX(hType, center, radius, index):
+	vec[0];
+	angle_deg = 60 * index + hType; # 0 if Flat, 30 if Pointy
+	angle_rad = math.pi / 180 * angle_deg;
+	vec[0].x = center.x + size * cos(angle_rad);
+	vec[0].y = center.y + size * sin(angle_rad);
+	return vec;
+
+# param: hType, hexType, 0 for Flat topped, 30 if Pointy topped
+# param: center, Vector2Point, hex center
+# param: radius, size of hex
+# returns: Vector2[] hex corners
+def GenerateVectorsHEX(hType, center, radius):
+	vec[0];
+	for val in range(6):
+		angle_deg = 60 * val + hType; # 0 if Flat, 30 if Pointy
+		angle_rad = math.pi / 180 * angle_deg;
+		vec[val].x = center.x + size * cos(angle_rad);
+		vec[val].y = center.y + size * sin(angle_rad);
+	return vec;
   
