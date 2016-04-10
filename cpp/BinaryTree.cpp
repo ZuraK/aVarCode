662978f940c8e56213b3b514399878149f515117
Michael Åberg <ZuraK@users.noreply.github.com>
2016-04-10 07:06:31 +0200

    Create BinaryTree.cpp

---
+++ cpp/BinaryTree.cpp
@@ -0,0 +1,17 @@
+
+namespace Util
+{
+  struct BinaryTreeNode
+  {
+    int Key;
+    BinaryTreeNode *Left;
+    BinaryTreeNode *Right;
+  };
+  
+  class BinaryTree
+  {
+    public:
+      BinaryTree();
+      ~BinaryTree();
+  };
+}
