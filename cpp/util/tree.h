f2a71e8c557d25df3d31ad7f4c93d9ae0204f1a2
Michael Åberg <ZuraK@users.noreply.github.com>
2016-04-28 04:50:02 +0200

    Update tree.h

--- cpp/util/tree.h
+++ cpp/util/tree.h
@@ -20,4 +20,32 @@ namespace Util
       Search();
       Clear();
   };
+  
+  struct BinaryTreeNode
+  {
+    int KeyValue;
+    BinaryTreeNode *Left;
+		BinaryTreeNode *Right;
+  };
+  
+  class BinaryTree
+	{
+		public:
+			BinaryTree();
+			~BinaryTree();
+
+			void insert(int key);
+			BinaryTreeNode *search(int key);
+			void destroy_tree();
+
+		private:
+			void destroy_tree(BinaryTreeNode *leaf);
+			void insert(int key, BinaryTreeNode *leaf);
+			BinaryTreeNode *search(int key, BinaryTreeNode *leaf);
+			BinaryTreeNode *root;
+	};
+  
+  
+  
+  
 }
